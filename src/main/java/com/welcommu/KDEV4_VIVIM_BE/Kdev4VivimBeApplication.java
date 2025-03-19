3ef8d2ed87635888ec35be7790fc8c26e5f1436c
EOJIN <feshed528@gmail.com>
2025-03-19 17:08:40 +0900

    [init] 스프링부트 프로젝트 초기 환경설정

---
+++ src/main/java/com/welcommu/KDEV4_VIVIM_BE/Kdev4VivimBeApplication.java
@@ -0,0 +1,13 @@
+package com.welcommu.KDEV4_VIVIM_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdev4VivimBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kdev4VivimBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welcommu/KDEV4_VIVIM_BE/Kdev4VivimBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welcommu.KDEV4_VIVIM_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kdev4VivimBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
