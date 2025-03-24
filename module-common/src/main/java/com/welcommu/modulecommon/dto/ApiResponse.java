9efa72ebd7e4e9ed3dbf1333a2d4d43332139b3a
EOJIN <feshed528@gmail.com>
2025-03-24 18:38:26 +0900

    [feature] 요청 응답을 반환하는 공통 DTO 추가 (ApiResponse)

---
+++ module-common/src/main/java/com/welcommu/modulecommon/dto/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.welcommu.modulecommon.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponse {
+
+    private int statusCode;
+    private String statusMessage;
+
+    public ApiResponse(int statusCode, String statusMessage) {
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    }
+}
