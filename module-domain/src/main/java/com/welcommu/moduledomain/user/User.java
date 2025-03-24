0d422cf79fe82d60dc8462adaaaf939a4aeddc0c
정승민 <67104714+Codemaniac7@users.noreply.github.com>
2025-03-24 17:56:29 +0900

    [feature] #20 Swagger 기본 설정 추가, Spring Security 기본 설정 추가, UserEntity 어노테이션 수정 (#22)
    * companyEntity
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정

--- module-domain/src/main/java/com/welcommu/moduledomain/user/User.java
+++ module-domain/src/main/java/com/welcommu/moduledomain/user/User.java
@@ -1,20 +1,31 @@
 package com.welcommu.moduledomain.user;
 
 import com.welcommu.moduledomain.company.Company;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 50, nullable = false)
     private String name;
+
+    @Column(length = 100, nullable = false)
     private String email;
+
+    @Column(length = 20, nullable = false)
     private String phone;
+
     private LocalDateTime passwordModifiedAt;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
