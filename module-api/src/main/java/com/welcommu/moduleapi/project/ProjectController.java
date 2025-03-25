01a33c450cb347101db39e2c57a17d19b36f036f
HoyiTT <pjs9177@naver.com>
2025-03-25 18:57:06 +0900

    [feature] #18 프로젝트 생성 수정 추가

--- module-api/src/main/java/com/welcommu/moduleapi/project/ProjectController.java
+++ module-api/src/main/java/com/welcommu/moduleapi/project/ProjectController.java
@@ -1,9 +1,12 @@
 package com.welcommu.moduleapi.project;
 
+import com.welcommu.moduleservice.project.ProjectCreateRequestDto;
 import com.welcommu.moduleservice.project.ProjectService;
+import com.welcommu.moduleservice.project.ProjectUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -11,5 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController {
 
     private final ProjectService projectService;
+
+    // 프로젝트 생성
+    @PostMapping
+    public ResponseEntity<Void> createProject(@RequestBody ProjectCreateRequestDto dto) {
+        projectService.createProject(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    // 프로젝트 수정
+    @PutMapping("/{projectId}")
+    public ResponseEntity<Void> updateProject(@PathVariable Long projectId,
+                                              @RequestBody ProjectUpdateRequestDto dto) {
+        projectService.updateProject(projectId, dto);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
--- module-service/src/main/java/com/welcommu/moduleservice/project/ProjectService.java
+++ module-service/src/main/java/com/welcommu/moduleservice/project/ProjectService.java
@@ -1,12 +1,89 @@
 package com.welcommu.moduleservice.project;
 
+import com.welcommu.moduledomain.project.Project;
+import com.welcommu.moduledomain.project.ProjectUser;
+import com.welcommu.moduledomain.user.User;
 import com.welcommu.modulerepository.project.ProjectRepository;
+import com.welcommu.modulerepository.project.ProjectUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+
+    private final ProjectUserRepository projectUserRepository;
+
+    @Transactional
+    public void createProject(ProjectCreateRequestDto dto) {
+        User creator = userRepository.findById(dto.getCreatorId())
+                .orElseThrow(() -> new IllegalArgumentException("생성자 없음"));
+
+        Project project = Project.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .status(dto.getProjectStatus())
+                .startDate(dto.getStartDate())
+                .endDate(dto.getEndDate())
+                .createdAt(LocalDateTime.now())
+                .isDeleted(false)
+                .creator(creator)
+                .build();
+
+        projectRepository.save(project);
+
+        for (ProjectCreateRequestDto.UserRoleDto userRoleDto : dto.getUsers()) {
+            User user = userRepository.findById(userRoleDto.getUserId())
+                    .orElseThrow(() -> new IllegalArgumentException("해당 유저 없음"));
+
+            ProjectUser projectUser = ProjectUser.builder()
+                    .project(project)
+                    .user(user)
+                    .projectUserRole(userRoleDto.getRole())
+                    .build();
+
+            projectUserRepository.save(projectUser);
+        }
+    }
+
+    @Transactional
+    public void updateProject(Long projectId, ProjectUpdateRequestDto dto) {
+        Project original = projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 프로젝트 없음"));
+
+        User modifier = userRepository.findById(dto.getModifierId())
+                .orElseThrow(() -> new IllegalArgumentException("수정자 없음"));
+
+        Project updated = original.toBuilder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .status(dto.getProjectStatus())
+                .startDate(dto.getStartDate())
+                .endDate(dto.getEndDate())
+                .modifiedAt(LocalDateTime.now())
+                .modifier(modifier)
+                .build();
+
+        projectRepository.save(updated);
+        projectUserRepository.deleteByProject(original);
+
+        for (ProjectUpdateRequestDto.UserRoleDto userRoleDto : dto.getUsers()) {
+            User user = userRepository.findById(userRoleDto.getUserId())
+                    .orElseThrow(() -> new IllegalArgumentException("해당 유저 없음"));
+
+            ProjectUser projectUser = ProjectUser.builder()
+                    .project(updated)
+                    .user(user)
+                    .projectUserRole(userRoleDto.getRole())
+                    .build();
+
+            projectUserRepository.save(projectUser);
+        }
+    }
 
 }
