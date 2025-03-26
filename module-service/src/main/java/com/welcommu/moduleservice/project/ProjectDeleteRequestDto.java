1f4b1ded252a578434a753a6fbccc1ef5aed550e
HoyiTT <pjs9177@naver.com>
2025-03-26 15:48:03 +0900

    [feature] #18 프로젝트 CRUD DTO 적용

--- module-service/src/main/java/com/welcommu/moduleservice/project/ProjectDeleteRequestDto.java
+++ module-service/src/main/java/com/welcommu/moduleservice/project/ProjectDeleteRequestDto.java
@@ -1,11 +1,14 @@
 package com.welcommu.moduleservice.project;
 
+import com.welcommu.moduledomain.project.Project;
 import com.welcommu.moduledomain.project.ProjectStatus;
 import com.welcommu.moduledomain.project.ProjectUserRole;
+import com.welcommu.moduledomain.user.User;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -21,12 +24,25 @@ public class ProjectDeleteRequestDto {
 
     private Long modifierId; // 수정자 ID
 
-    private List<UserRoleDto> users; // 수정 후의 참여자 목록
+    private List<ProjectUserRequestDto> users; // 수정 후의 참여자 목록
 
     @Getter
     @Setter
     public static class UserRoleDto {
         private Long userId;
         private ProjectUserRole role;
+    }
+
+    public Project toEntity(User deleter){
+        return Project.builder()
+                .name(this.getName())
+                .description(this.getDescription())
+                .status(this.getProjectStatus())
+                .startDate(this.getStartDate())
+                .endDate(this.getEndDate())
+                .deletedAt(LocalDateTime.now())
+                .isDeleted(true)
+                .deleter(deleter)
+                .build();
     }
 }
