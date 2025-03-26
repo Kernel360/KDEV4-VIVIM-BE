375f08a73763035ea91d2a2908d1863534fda7d4
HoyiTT <pjs9177@naver.com>
2025-03-26 13:22:13 +0900

    feature] #18 프로젝트 엔드포인트 수정 및 dto 팩토리 메서드 적용

---
+++ module-service/src/main/java/com/welcommu/moduleservice/project/ProjectDeleteRequestDto.java
@@ -0,0 +1,32 @@
+package com.welcommu.moduleservice.project;
+
+import com.welcommu.moduledomain.project.ProjectStatus;
+import com.welcommu.moduledomain.project.ProjectUserRole;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class ProjectDeleteRequestDto {
+
+    private String name;
+    private String description;
+    private ProjectStatus projectStatus;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private Long modifierId; // 수정자 ID
+
+    private List<UserRoleDto> users; // 수정 후의 참여자 목록
+
+    @Getter
+    @Setter
+    public static class UserRoleDto {
+        private Long userId;
+        private ProjectUserRole role;
+    }
+}
