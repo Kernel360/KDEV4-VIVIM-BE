4a17c8d24b7ff77124b3e8093911261003cfd51e
HoyiTT <pjs9177@naver.com>
2025-03-26 15:48:03 +0900

    [feature] #18 프로젝트 CRUD DTO 적용

--- module-service/src/main/java/com/welcommu/moduleservice/project/ProjectUpdateRequestDto.java
+++ module-service/src/main/java/com/welcommu/moduleservice/project/ProjectUpdateRequestDto.java
@@ -1,11 +1,14 @@
 package com.welcommu.moduleservice.project;
 
+import com.welcommu.moduledomain.project.Project;
 import com.welcommu.moduledomain.project.ProjectStatus;
 import com.welcommu.moduledomain.project.ProjectUserRole;
+import com.welcommu.moduledomain.user.User;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -21,12 +24,22 @@ public class ProjectUpdateRequestDto {
 
     private Long modifierId; // 수정자 ID
 
-    private List<UserRoleDto> users; // 수정 후의 참여자 목록
+    private List<ProjectUserRequestDto> users;
 
     @Getter
     @Setter
     public static class UserRoleDto {
         private Long userId;
         private ProjectUserRole role;
+    }
+
+    public void applyTo(Project project, User modifier) {
+        project.setName(this.name);
+        project.setDescription(this.description);
+        project.setStatus(this.projectStatus);
+        project.setStartDate(this.startDate);
+        project.setEndDate(this.endDate);
+        project.setModifiedAt(LocalDateTime.now());
+        project.setModifier(modifier);
     }
 }
