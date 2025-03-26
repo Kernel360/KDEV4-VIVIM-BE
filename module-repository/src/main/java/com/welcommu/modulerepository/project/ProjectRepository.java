0a39d222acbbfa55bd1633491a378faa24c77334
JUNSEO PARK <pjs9177@naver.com>
2025-03-26 17:39:40 +0900

    [feature] #18 프로젝트 CRUD 추가 (#30)
    * [feat] #18 프로젝트 생성 기능 초안 구현
    
    * [feature] #18 프로젝트 엔티티 재 설계 및 프로젝트 유저 생성
    
    * [feature] #18 현재 불필요한 ProjectRequest 삭제
    
    * [feat] #18 프로젝트 생성 기능 초안 구현
    
    * [feature] #18 프로젝트 엔티티 재 설계 및 프로젝트 유저 생성
    
    * [feature] #18 프로젝트 생성 수정 추가
    
    * [feature] #18 @builder 어노테이션 옵션추가
    
    * feature] #18 프로젝트 엔드포인트 수정 및 dto 팩토리 메서드 적용
    
    * feature] #18 프로젝트 응답 공통  DTO 적용
    
    * [feature] #18 프로젝트 CRUD DTO 적용
    
    * [featur] #18 DTO 패키지 분리 및 미사용 클래스 제거
    
    * [feature] #18 project Entity Setter 제거
    
    * [feature] #18 Setter 제거로 인한 delete 서비스 로직 변경
    
    * [feature] #18 Project Update 메서드 컨벤션 리팩터링

--- module-repository/src/main/java/com/welcommu/modulerepository/project/ProjectRepository.java
+++ module-repository/src/main/java/com/welcommu/modulerepository/project/ProjectRepository.java
@@ -4,9 +4,6 @@ import com.welcommu.moduledomain.project.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Optional<Project> findByName(String name);
 }
