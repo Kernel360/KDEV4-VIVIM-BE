005e35056fa8cfe03cc201b093a176cf4a50bf1e
정승민 <67104714+Codemaniac7@users.noreply.github.com>
2025-03-28 17:06:28 +0900

    [feature] Company, User CRUD 기능 구현 및 Security, Swagger 수정 (#33)
    * companyEntity
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정
    
    * UserEntity password 필드 추가 + CustomUserDetails 추가
    
    * User CRUD 추가 및 CompanyServive,CompanyRepository추가
    
    * implementation jpa 중복 제거
    
    실수로 또 추가했네요
    
    * [feature] UserEntity password 필드 추가 + CustomUserDetails 추가 (#26)
    
    * companyEntity
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정
    
    * UserEntity password 필드 추가 + CustomUserDetails 추가
    
    * User CRUD 추가 및 CompanyServive,CompanyRepository추가
    
    * implementation jpa 중복 제거
    
    실수로 또 추가했네요
    
    * build.gradle중복 제거
    
    * reset
    
    * CompanyCRUD 추가 ë[feature]° User 회원가입 및 로그인 í수정 및 테스트 확인
    
    * CompanyCRUD 추가 [feature] User 회원가입 수정 및 로그인
    
    * companyEntity
    
    # Conflicts:
    #	module-domain/src/main/java/com/welcommu/moduledomain/company/Company.java
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User CRUD 추가 및 CompanyServive,CompanyRepository추가
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정
    
    * build.gradle중복 제거
    
    * reset
    
    * CompanyCRUD 추가 [feature] User 회원가입 수정 및 로그인
    
    * [feature]company&user CRUD 수정 및 Swagger, Security 수정
    
    * [feature]securityConfig설정 수정, log.info, user 필드 수정
    
    * [refactor] 필요없는 주석 삭제
    
    * 불필요한 gradle 설정 삭제 및 ë간소화
    
    ---------
    
    Co-authored-by: EOJIN <122079064+song-eojin@users.noreply.github.com>

--- module-common/src/main/java/com/welcommu/modulecommon/exception/CustomErrorCode.java
+++ module-common/src/main/java/com/welcommu/modulecommon/exception/CustomErrorCode.java
@@ -1,11 +1,13 @@
-package com.welcommu.exception;
+package com.welcommu.modulecommon.exception;
 
 import lombok.Getter;
 
 @Getter
 public enum CustomErrorCode {
     //project progress
-    NOT_FOUND_PROGRESS("PG001", "찾을 수 없는 프로젝트 단계입니다.");
+    NOT_FOUND_PROGRESS("PG001", "찾을 수 없는 프로젝트 단계입니다."),
+    // 회사 관련 에러 코드
+    NOT_FOUND_COMPANY("COMPANY001", "회사를 찾을 수 없습니다.");
 
     private final String code;
     private final String errorMessage;
