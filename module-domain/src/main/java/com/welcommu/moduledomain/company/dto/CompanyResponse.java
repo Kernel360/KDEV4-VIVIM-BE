5d5a7d886df560a649793622df0a5fccd297a4cf
정승민 <67104714+Codemaniac7@users.noreply.github.com>
2025-03-30 18:27:16 +0900

    [feature] Company별 직원조회 추가 (#43)
    * companyEntity
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정
    
    * UserEntity password 필드 추가 + CustomUserDetails 추가
    
    * User CRUD 추가 및 CompanyServive,CompanyRepository추가
    
    * implementation jpa 중복 제거
    
    실수로 또 추가했네요
    
    * [feature] UserEntity password 필드 추가 + CustomUserDetails 추가 (#26)
    
    * companyEntity
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정
    
    * UserEntity password 필드 추가 + CustomUserDetails 추가
    
    * User CRUD 추가 및 CompanyServive,CompanyRepository추가
    
    * implementation jpa 중복 제거
    
    실수로 또 추가했네요
    
    * build.gradle중복 제거
    
    * reset
    
    * CompanyCRUD 추가 ë[feature]° User 회원가입 및 로그인 í수정 및 테스트 확인
    
    * CompanyCRUD 추가 [feature] User 회원가입 수정 및 로그인
    
    * companyEntity
    
    # Conflicts:
    #	module-domain/src/main/java/com/welcommu/moduledomain/company/Company.java
    
    * userEntity 작성
    
    * springSecurity&Swagger
    
    * UserEntity수정
    
    * User CRUD 추가 및 CompanyServive,CompanyRepository추가
    
    * User Entity 어노테이션 수정
    
    User Entity 어노테이션 수정
    @Column 추가 및 @Data 어노테이션 수정
    
    * build.gradle중복 제거
    
    * reset
    
    * CompanyCRUD 추가 [feature] User 회원가입 수정 및 로그인
    
    * [feature]company&user CRUD 수정 및 Swagger, Security 수정
    
    * [feature]securityConfig설정 수정, log.info, user 필드 수정
    
    * [refactor] 필요없는 주석 삭제
    
    * 불필요한 gradle 설정 삭제 및 ë간소화
    
    * ë¶ã불필요한 내용 DTOì° 및 NotNull 추가 등
    
    * 불필요한 ë DTO 및 내용 간소화
    
    * [feature]Company CRUD 수정
    
    * [feature] 회사별 직원조회 추가
    
    * [feature]회사별 직원 조회 추가
    
    * CRUD ompany[feature] User 조회 수정 및 ErrorCode추가
    
    ---------
    
    Co-authored-by: EOJIN <122079064+song-eojin@users.noreply.github.com>

--- module-domain/src/main/java/com/welcommu/moduledomain/company/dto/CompanyResponse.java
+++ module-domain/src/main/java/com/welcommu/moduledomain/company/dto/CompanyResponse.java
@@ -1,17 +1,25 @@
 package com.welcommu.moduledomain.company.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CompanyResponse {
     private Long id;
     private String name;
     private String address;
-    private String phoneNumber;
+    private String phone;
     private String email;
+    private String bussinessNumber;
 
-    public CompanyResponse(Long id, String name, String address, String phoneNumber, String email) {
+    public CompanyResponse(Long id, String name, String address, String phone, String email, String bussinessNumber) {
         this.id = id;
         this.name = name;
         this.address = address;
-        this.phoneNumber = phoneNumber;
+        this.phone = phone;
         this.email = email;
+        this.bussinessNumber = bussinessNumber;
     }
 }
