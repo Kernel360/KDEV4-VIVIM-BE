5fb942193e742b562974854abe7ffb6463317763
moosoobi <tmxkcksgh@gmail.com>
2025-03-31 11:34:34 +0900

    [feat] repository필요없는 import제거

--- module-repository/src/main/java/com/welcommu/modulerepository/projectpost/repository/ProjectPostCommentRepository.java
+++ module-repository/src/main/java/com/welcommu/modulerepository/projectpost/repository/ProjectPostCommentRepository.java
@@ -1,6 +1,5 @@
 package com.welcommu.modulerepository.projectpost.repository;
 
-import com.welcommu.moduledomain.projectpost.entity.ProjectPost;
 import com.welcommu.moduledomain.projectpost.entity.ProjectPostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
