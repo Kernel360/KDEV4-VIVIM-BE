65e987d2dd5db53e2584541a39915f1206d72935
EOJIN <122079064+song-eojin@users.noreply.github.com>
2025-04-02 14:33:47 +0900

    [feature] #14 ProjectProgress 수정, 삭제, 전체조회 기능 구현 (#42)
    * [feature] ProjectProgress 수정, 삭제, 전체조회 기능 구현 (Swagger 테스트 완료)
    
    * [refactor] Project Progress 전체조회 기능에서 필요없는 매개변수 삭제
    
    * [refactor] ProjectPost 패키지 컨벤션 맞추기
    
    * [refactor] exception 패키지 위치 수정
    
    * [bug] ProjectProgress Update API에서 변경사항이 저장되지 않는 문제 수정
    
    * [test] ProjectProgress Service 단위 테스트 (JUnit, Mockito)
    
    * [feature] ProjectProgress GetList API는 단순 조회 작업이므로 Transaction readOnly 설정 제거
    
    * [feature] Project 생성될 때 Default ProjectProgress 자동으로 들어가는 기능 구현
    
    * [bug] ProjectProgress Create에서 maxPosition 읽어오는 JPQL 수정
    
    * [feature] RequiredAurgsConstructor를 AllArgsConstructor로 변경
    
    * [refactor] 수정 메서드명 컨벤션 맞추기 (update -> modify)
    
    * [refactor] Project Service에서 ProjectProgressRepository 접근제어자 변경 (protected → private)
    
    * [test] 실제 DB 영향 반영: 테스트 예상값 변경
    
    * [test] ProjectProgressServiceTest: setUp() 구현 방식 변경 (set -> builder)

--- module-api/src/main/java/com/welcommu/moduleapi/projectprogress/ProjectProgressController.java
+++ module-api/src/main/java/com/welcommu/moduleapi/projectprogress/ProjectProgressController.java
@@ -1,37 +1,82 @@
 package com.welcommu.moduleapi.projectprogress;
 
 import com.welcommu.modulecommon.dto.ApiResponse;
-import com.welcommu.moduledomain.user.CustomUserDetails;
 import com.welcommu.moduleservice.projectProgess.ProjectProgressService;
 import com.welcommu.moduleservice.projectProgess.dto.ProgressCreateRequest;
+import com.welcommu.moduleservice.projectProgess.dto.ProgressListResponse;
+import com.welcommu.moduleservice.projectProgess.dto.ProgressModifyRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/api/projects")
+@AllArgsConstructor
 @Tag(name = "프로젝트 단계 API", description = "프로젝트 단계를 셍성, 수정, 삭제, 이동시킬 수 있습니다.")
 public class ProjectProgressController {
 
-    private ProjectProgressService progressService;
+    private final ProjectProgressService progressService;
 
     @PostMapping("/{projectId}/progress")
     @Operation(summary = "프로젝트 단계 생성")
     public ResponseEntity<ApiResponse> createProjectProgress(
-        @AuthenticationPrincipal CustomUserDetails userDetails,
         @PathVariable Long projectId,
         @RequestBody ProgressCreateRequest request
     ) {
 
-        progressService.create(userDetails.getUser(), projectId, request);
+        log.info("프로젝트 단계 생성 요청: projectId={}, request={}", projectId, request);
+        progressService.createProgress(projectId, request);
+        log.info("프로젝트 단계 생성 완료: projectId={}", projectId);
         return ResponseEntity.ok().body(new ApiResponse(HttpStatus.CREATED.value(),"프로젝트 단계 생성을 성공했습니다."));
     }
 
+    @PutMapping("/{projectId}/progress/{progressId}")
+    @Operation(summary = "프로젝트 단계 수정")
+    public ResponseEntity<ApiResponse> updateProgress(
+        @PathVariable Long projectId,
+        @PathVariable Long progressId,
+        @RequestBody ProgressModifyRequest request
+    ) {
+
+        log.info("프로젝트 단계 수정 요청: projectId={}, progressId={}, request={}", projectId, progressId, request);
+        progressService.modifyProgress(projectId, progressId, request);
+        log.info("프로젝트 단계 수정 완료: projectId={}, progressId={}", projectId, progressId);
+        return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "프로젝트 단계 수정 성공"));
+    }
+
+    @DeleteMapping("/{projectId}/progress/{progressId}")
+    @Operation(summary = "프로젝트 단계 삭제")
+    public ResponseEntity<ApiResponse> deleteProgress(
+        @PathVariable Long projectId,
+        @PathVariable Long progressId
+    ) {
+
+        log.info("프로젝트 단계 삭제 요청: projectId={}, progressId={}", projectId, progressId);
+        progressService.deleteProgress(projectId, progressId);
+        log.info("프로젝트 단계 삭제 완료: projectId={}, progressId={}", projectId, progressId);
+        return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "프로젝트 단계 삭제 성공"));
+    }
+
+    @GetMapping("/{projectId}/progress")
+    @Operation(summary = "프로젝트 단계 전체조회")
+    public ResponseEntity<ProgressListResponse> getProgressList(
+        @PathVariable Long projectId) {
+
+        log.info("프로젝트 단계 전체 조회 요청: projectId={}", projectId);
+        ProgressListResponse progressList = progressService.getProgressList(projectId);
+        log.info("프로젝트 단계 전체 조회 완료: projectId={}, count={}", projectId, progressList.getProgressList().size());
+        return ResponseEntity.ok(progressList);
+    }
 }
