1c1822a6b8f9e6d3d6b4f5a88d054ca4e7b159bb
HoyiTT <pjs9177@naver.com>
2025-04-03 09:48:04 +0900

    [feature] Auth Controller 매핑 도메인 변경

--- module-api/src/main/java/com/welcommu/moduleapi/auth/AuthController.java
+++ module-api/src/main/java/com/welcommu/moduleapi/auth/AuthController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final JwtTokenHelper jwtTokenHelper;
