79833bcf9dd13e6e8e3ba75ae0d14b59e65980f4
moosoobi <tmxkcksgh@gmail.com>
2025-04-03 11:39:12 +0900

    [feature] Swagger Tag, Operation어노테이션 추가

--- module-api/src/main/java/com/welcommu/moduleapi/projectpost/ProjectPostCommentController.java
+++ module-api/src/main/java/com/welcommu/moduleapi/projectpost/ProjectPostCommentController.java
@@ -3,6 +3,8 @@ package com.welcommu.moduleapi.projectpost;
 import com.welcommu.modulecommon.dto.ApiResponse;
 import com.welcommu.moduleservice.projectpost.dto.*;
 import com.welcommu.moduleservice.projectpost.ProjectPostCommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +18,12 @@ import static com.welcommu.modulecommon.util.IpUtil.getClientIp;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts/{postId}/comments")
+@Tag(name = "게시글 댓글 API", description = "댓글을 셍성, 전체 조회, 수정, 삭제, 이동시킬 수 있습니다.")
 public class ProjectPostCommentController {
     private final ProjectPostCommentService projectPostCommentService;
 
     @PostMapping
+    @Operation(summary = "댓글 생성")
     public ResponseEntity<ApiResponse> createComment(@PathVariable Long postId, @RequestBody ProjectPostCommentRequest request, HttpServletRequest httpRequest){
         String clientIp = getClientIp(httpRequest);
         projectPostCommentService.createComment(postId,request, clientIp);
@@ -27,12 +31,14 @@ public class ProjectPostCommentController {
     }
 
     @PutMapping("/{commentId}")
+    @Operation(summary = "댓글 수정")
     public ResponseEntity<ApiResponse> modifyComment(@PathVariable Long postId, @PathVariable Long commentId, @RequestBody ProjectPostCommentRequest request) {
         projectPostCommentService.modifyComment(postId, commentId, request);
         return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "게시글이 수정되었습니다."));
     }
 
     @GetMapping
+    @Operation(summary = "댓글 목록 조회")
     public ResponseEntity<List<ProjectPostCommentListResponse>> getCommentList(@PathVariable Long postId) {
         List<ProjectPostCommentListResponse> resultList = projectPostCommentService.getCommentList(postId);
         return ResponseEntity.ok(resultList);
@@ -41,6 +47,7 @@ public class ProjectPostCommentController {
 
 
     @DeleteMapping("/{commentId}")
+    @Operation(summary = "댓글 삭제")
     public ResponseEntity<ApiResponse> deleteComment(@PathVariable Long postId, @PathVariable Long commentId) {
         projectPostCommentService.deleteComment(postId, commentId);
         return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "게시글이 삭제되었습니다."));
--- module-api/src/main/java/com/welcommu/moduleapi/projectpost/ProjectPostController.java
+++ module-api/src/main/java/com/welcommu/moduleapi/projectpost/ProjectPostController.java
@@ -6,6 +6,8 @@ import com.welcommu.moduleservice.projectpost.dto.ProjectPostListResponse;
 import com.welcommu.moduleservice.projectpost.dto.ProjectPostRequest;
 import com.welcommu.moduleservice.projectpost.ProjectPostService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
 import lombok.RequiredArgsConstructor;
@@ -20,12 +22,14 @@ import static com.welcommu.modulecommon.util.IpUtil.getClientIp;
 @RestController
 @RequestMapping("/api/projects/{projectId}/posts")
 @RequiredArgsConstructor
+@Tag(name = "게시글 API", description = "게시글을 셍성, 전체 조회, 상세 조회, 수정, 삭제, 이동시킬 수 있습니다.")
 public class ProjectPostController {
     private final ProjectPostService projectPostService;
 
 
 
     @PostMapping
+    @Operation(summary = "게시글 생성")
     public ResponseEntity<ApiResponse> createPost(@PathVariable Long projectId, @RequestBody ProjectPostRequest request, HttpServletRequest httpRequest) {
         String clientIp = getClientIp(httpRequest);
         projectPostService.createPost(projectId, request, clientIp);
@@ -33,24 +37,28 @@ public class ProjectPostController {
     }
 
     @PutMapping("/{postId}")
+    @Operation(summary = "게시글 수정")
     public ResponseEntity<ApiResponse> modifyPost(@PathVariable Long projectId, @PathVariable Long postId, @RequestBody ProjectPostRequest request) {
         projectPostService.modifyPost(projectId, postId, request);
         return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "게시글이 수정되었습니다."));
     }
 
     @GetMapping
+    @Operation(summary = "게시글 목록 조회")
     public ResponseEntity<List<ProjectPostListResponse>> getPostList(@PathVariable Long projectId) {
         List<ProjectPostListResponse> resultList = projectPostService.getPostList(projectId);
         return ResponseEntity.ok(resultList);
     }
 
     @GetMapping("/{postId}")
+    @Operation(summary = "게시글 상세 조회")
     public ResponseEntity<ProjectPostDetailResponse> getPostDetail(@PathVariable Long projectId, @PathVariable Long postId) {
         ProjectPostDetailResponse result = projectPostService.getPostDetail(projectId, postId);
         return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("/{postId}")
+    @Operation(summary = "게시글 삭제")
     public ResponseEntity<ApiResponse> deletePost(@PathVariable Long projectId, @PathVariable Long postId) {
         projectPostService.deletePost(projectId, postId);
         return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "게시글이 삭제되었습니다."));
