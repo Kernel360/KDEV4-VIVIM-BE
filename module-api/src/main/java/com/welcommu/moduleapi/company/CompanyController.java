d135f2407b2793371e4dccc42b452c5c5b3dee70
HoyiTT <pjs9177@naver.com>
2025-04-07 14:17:32 +0900

    [feature] Company 관련 swaager 설명 추가

--- module-api/src/main/java/com/welcommu/moduleapi/company/CompanyController.java
+++ module-api/src/main/java/com/welcommu/moduleapi/company/CompanyController.java
@@ -6,6 +6,8 @@ import com.welcommu.moduleservice.company.dto.CompanyRequest;
 import com.welcommu.moduleservice.company.dto.CompanyResponse;
 import com.welcommu.moduleservice.company.CompanyManagementService;
 import com.welcommu.moduleservice.user.dto.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +23,14 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/companies")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "회사 API", description = "회사를 생성, 수정, 삭제시킬 수 있습니다.")
 public class CompanyController {
 
     private final CompanyManagementService companyManagementService;
 
+
     @PostMapping
+    @Operation(summary = "회사를 생성합니다..")
     public ResponseEntity<ApiResponse> createCompany(@RequestBody CompanyRequest companyRequest) {
         log.info("Received company: {}", companyRequest);
 
@@ -40,6 +45,7 @@ public class CompanyController {
     }
 
     @GetMapping
+    @Operation(summary = "전체 회사 목록을 조회합니다.")
     public List<CompanyResponse> getCompanyList() {
         log.info("전체 회사 조회 API 호출됨.");
         List<Company> companies = companyManagementService.getAllCompany();
@@ -49,12 +55,14 @@ public class CompanyController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "회사 id를 바탕으로 회사를 개별 조회합니다.")
     public ResponseEntity<CompanyResponse> getCompanyById(@PathVariable Long id) {
         Optional<Company> company = companyManagementService.getCompanyById(id);
         return company.map(c -> ResponseEntity.ok(CompanyResponse.from(c)))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "id를 바탕으로 해당 회사에 소속된 유저를 전체 조회합니다.")
     @GetMapping("/{companyId}/employees")
     public ResponseEntity<ApiResponse> getEmployeesByCompany(@PathVariable Long companyId) {
         log.info("회사 ID {}에 속한 직원 목록 조회 API 호출됨.", companyId);
@@ -69,6 +77,7 @@ public class CompanyController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "id를 바탕으로 회사를 수정합니다.")
     public ResponseEntity<Company> updateCompany(@PathVariable Long id, @RequestBody Company updatedCompany) {
         try {
             Company company = companyManagementService.updateCompany(id, updatedCompany);
@@ -78,6 +87,7 @@ public class CompanyController {
         }
     }
 
+    @Operation(summary = "id를 바탕으로 회사를 삭제합니다.(Hard Delete)")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
         companyManagementService.deleteCompany(id);
