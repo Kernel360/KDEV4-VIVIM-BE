2a74fb56b59997f709f581c0b9b8fc13ff9e7ec3
moosoobi <tmxkcksgh@gmail.com>
2025-04-08 20:21:10 +0900

    [feature] 댓글, 게시글 생성시 생성자 name, role추가

--- module-domain/src/main/java/com/welcommu/moduledomain/projectpost/ProjectPostComment.java
+++ module-domain/src/main/java/com/welcommu/moduledomain/projectpost/ProjectPostComment.java
@@ -1,5 +1,6 @@
 package com.welcommu.moduledomain.projectpost;
 
+import com.welcommu.moduledomain.company.CompanyRole;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
@@ -35,11 +36,11 @@ public class ProjectPostComment {
     @Column(name = "creator_id", nullable = false)
     private Long creatorId;
 
-    @Column(name="modifier_id")
-    private Long modifierId;
+    @Column(name="creator_name")
+    private String creatorName;
 
-    @Column(name="deleter_id")
-    private Long deleterId;
+    @Column(name="creator_role")
+    private String creatorRole;
 
     @Column(name = "project_post_id", nullable = false)
     private Long projectPostId;
@@ -47,23 +48,12 @@ public class ProjectPostComment {
     @Column(name = "parent_id")
     private Long parentId;
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
+    public void setContent(String content) { this.content = content; }
+    public void setCreatedAt(LocalDateTime createdAt) {}
     public void setModifiedAt() {
         this.modifiedAt = LocalDateTime.now();
     }
-
-    public void setModifierId(Long modifierId) {
-        this.modifierId = modifierId;
-    }
-
     public void setDeletedAt() {
         this.deletedAt = LocalDateTime.now();
-    }
-
-    public void setDeleterId(Long deleterId) {
-        this.deleterId = deleterId;
     }
 }
