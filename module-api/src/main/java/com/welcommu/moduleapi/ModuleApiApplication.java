35fc93abc935cbbe49bee937664f16b45435bf48
HoyiTT <pjs9177@naver.com>
2025-04-15 18:28:40 +0900

    [fix] 불필요한 어노테이션 제거

--- module-api/src/main/java/com/welcommu/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/welcommu/moduleapi/ModuleApiApplication.java
@@ -3,22 +3,12 @@ package com.welcommu.moduleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(
-        scanBasePackages = {
-                "com.welcommu.moduleapi",
-                "com.welcommu.modulecommon",
-                "com.welcommu.modulerepository",
-                "com.welcommu.moduleservice"}
-)
+@SpringBootApplication(scanBasePackages = { "com.welcommu.moduleapi", "com.welcommu.modulecommon", "com.welcommu.modulerepository", "com.welcommu.moduleservice"})
 @EntityScan("com.welcommu.moduledomain")
 @EnableJpaRepositories("com.welcommu.modulerepository")
-@EnableAspectJAutoProxy
 public class ModuleApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ModuleApiApplication.class, args);
     }
