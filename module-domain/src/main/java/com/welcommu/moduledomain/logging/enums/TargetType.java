16b2d17416beb2f2ed8fc450691b214bbda73fb3
HoyiTT <pjs9177@naver.com>
2025-04-16 15:50:08 +0900

    [fix] TargetType Enum 수정

--- module-domain/src/main/java/com/welcommu/moduledomain/logging/enums/TargetType.java
+++ module-domain/src/main/java/com/welcommu/moduledomain/logging/enums/TargetType.java
@@ -1,6 +1,6 @@
 package com.welcommu.moduledomain.logging.enums;
 
 public enum TargetType {
-    USER, COMPANY, PROJECT, PHASE, CHECKLIST, POST, COMMENT
+    USER, COMPANY, PROJECT, PHASE, POST, COMMENT
 }
 
