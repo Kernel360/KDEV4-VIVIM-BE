46915a0b592ad5158aef875d4ba2fa080cc9ab75
moosoobi <tmxkcksgh@gmail.com>
2025-04-16 14:32:43 +0900

    [chore] #23 Google Code Style 적용
    # Conflicts:
    #	module-api/src/main/java/com/welcommu/moduleapi/ModuleApiApplication.java
    #	module-api/src/main/java/com/welcommu/moduleapi/project/ProjectController.java
    #	module-common/src/main/java/com/welcommu/modulecommon/filter/JwtAuthenticationFilter.java
    #	module-repository/src/main/java/com/welcommu/modulerepository/project/ProjectUserRepository.java
    #	module-service/src/main/java/com/welcommu/moduleservice/project/ProjectService.java
    #	module-service/src/test/java/com/welcommu/moduleservice/project/ProjectServiceTest.java
    
    # Conflicts:
    #	module-api/src/main/java/com/welcommu/moduleapi/checklist/ChecklistController.java
    #	module-api/src/main/java/com/welcommu/moduleapi/company/CompanyController.java
    #	module-api/src/main/java/com/welcommu/moduleapi/user/UserController.java
    #	module-common/src/main/java/com/welcommu/modulecommon/exception/CustomErrorCode.java
    #	module-service/src/main/java/com/welcommu/moduleservice/checklist/ChecklistService.java
    #	module-service/src/main/java/com/welcommu/moduleservice/checklist/dto/ChecklistListResponse.java
    #	module-service/src/main/java/com/welcommu/moduleservice/checklist/dto/ChecklistResponse.java
    #	module-service/src/main/java/com/welcommu/moduleservice/company/CompanyService.java
    #	module-service/src/main/java/com/welcommu/moduleservice/project/ProjectService.java
    #	module-service/src/main/java/com/welcommu/moduleservice/projectProgess/ProjectProgressService.java
    #	module-service/src/main/java/com/welcommu/moduleservice/user/UserService.java

---
+++ module-api/src/main/java/com/welcommu/moduleapi/checklist/ChecklistController.java
@@ -0,0 +1,62 @@
+package com.welcommu.moduleapi.checklist;
+
+import com.welcommu.modulecommon.dto.ApiResponse;
+import com.welcommu.moduleservice.checklist.ChecklistService;
+import com.welcommu.moduleservice.checklist.dto.ChecklistCreateRequest;
+import com.welcommu.moduleservice.checklist.dto.ChecklistListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/progress")
+@AllArgsConstructor
+@Tag(name = "체크리스트 API", description = "체크리스트를 생성, 수정, 삭제시킬 수 있습니다.")
+public class ChecklistController {
+
+    private ChecklistService checklistService;
+
+    @PostMapping("/{progressId}/checklists")
+    @Operation(summary = "체크리스트 생성")
+    public ResponseEntity<ApiResponse> createChecklist(@PathVariable Long progressId,
+        @RequestBody ChecklistCreateRequest request) {
+        checklistService.createChecklist(progressId, request);
+        return ResponseEntity.ok()
+            .body(new ApiResponse(HttpStatus.CREATED.value(), "체크리스트가 생성되었습니다."));
+    }
+
+    @PutMapping("/{checklistId}")
+    @Operation(summary = "체크리스트 수정")
+    public ResponseEntity<ApiResponse> modifyChecklist(
+        @PathVariable Long checklistId,
+        @RequestParam String name
+    ) {
+        checklistService.modifyChecklist(checklistId, name);
+        return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "체크리스트가 수정되었습니다."));
+    }
+
+    @DeleteMapping("/{checklistId}")
+    @Operation(summary = "체크리스트 삭제")
+    public ResponseEntity<ApiResponse> deleteChecklist(@PathVariable Long checklistId) {
+        checklistService.deleteChecklist(checklistId);
+        return ResponseEntity.ok().body(new ApiResponse(HttpStatus.OK.value(), "체크리스트가 삭제되었습니다."));
+    }
+
+    @GetMapping("/{progressId}/checklists")
+    @Operation(summary = "체크리스트 전체조회")
+    public ResponseEntity<ChecklistListResponse> getAllChecklist(@PathVariable Long progressId) {
+        ChecklistListResponse checkListResponse = checklistService.getAllChecklist(progressId);
+        return ResponseEntity.ok(checkListResponse);
+    }
+}
