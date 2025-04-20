da74f8dde93217c2d131d0902be80392e3728dec
EOJIN <feshed528@gmail.com>
2025-04-20 21:43:01 +0900

    [feature] ApprovalApprover 다른 도메인과의 연결부 구현 완료

--- module-repository/src/main/java/com/welcommu/modulerepository/approval/ApproverRepository.java
+++ module-repository/src/main/java/com/welcommu/modulerepository/approval/ApproverRepository.java
@@ -3,16 +3,19 @@ package com.welcommu.modulerepository.approval;
 import com.welcommu.moduledomain.approval.ApprovalApprover;
 import com.welcommu.moduledomain.approval.ApprovalProposal;
 import com.welcommu.moduledomain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApproverRepository extends JpaRepository<ApprovalApprover, Long> {
 
     // projectUser.user 로 내부 탐색
-    Optional<ApprovalApprover> findByApprovalProposalAndProjectUserUser(ApprovalProposal proposal, User user);
+    Optional<ApprovalApprover> findByApprovalProposalAndProjectUserUser(ApprovalProposal proposal,
+        User user);
+
     List<ApprovalApprover> findByApprovalProposal(ApprovalProposal proposal);
+
+    boolean existsByApprovalProposal(ApprovalProposal proposal);
 }
--- module-service/src/main/java/com/welcommu/moduleservice/approval/approvalApprover/dto/ApproverResponse.java
+++ module-service/src/main/java/com/welcommu/moduleservice/approval/approvalApprover/dto/ApproverResponse.java
@@ -1,11 +1,8 @@
 package com.welcommu.moduleservice.approval.approvalApprover.dto;
 
 import com.welcommu.moduledomain.approval.ApprovalApprover;
-import com.welcommu.moduledomain.projectUser.ProjectUser;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -13,15 +10,11 @@ public class ApproverResponse {
 
     private Long userId;
     private String name;
-    private boolean isApproved;
-    private LocalDateTime approvedAt;
 
     public static ApproverResponse of(ApprovalApprover approver) {
         return ApproverResponse.builder()
-                .userId(approver.getProjectUser().getUser().getId())
-                .name(approver.getProjectUser().getUser().getName())
-                .isApproved(approver.isApproved())
-                .approvedAt(approver.getApprovedAt())
-                .build();
+            .userId(approver.getProjectUser().getUser().getId())
+            .name(approver.getProjectUser().getUser().getName())
+            .build();
     }
 }
