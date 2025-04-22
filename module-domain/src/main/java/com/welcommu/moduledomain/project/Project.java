2e9fdee405ccc0fd30a46fd9a98534a260baa7f2
moosoobi <tmxkcksgh@gmail.com>
2025-04-22 17:10:18 +0900

    [feature] 충돌해결

--- module-domain/src/main/java/com/welcommu/moduledomain/project/Project.java
+++ module-domain/src/main/java/com/welcommu/moduledomain/project/Project.java
@@ -1,9 +1,18 @@
 package com.welcommu.moduledomain.project;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
@@ -22,6 +31,7 @@ public class Project {
     @Column(nullable = false)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Column(name = "start_date", nullable = false)
