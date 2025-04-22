e5f22b328354888cf0e1f233f6b6befcfff0e4f6
HoyiTT <pjs9177@naver.com>
2025-04-22 20:29:32 +0900

    [feature] Company  Search + Paging 적용

---
+++ module-repository/src/main/java/com/welcommu/modulerepository/company/CompanyRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.welcommu.modulerepository.company;
+
+import com.welcommu.moduledomain.company.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CompanyRepositoryCustom {
+    Page<Company> searchByConditions(String name, String businessNumber, String email, Boolean isDeleted, Pageable pageable);
+}
