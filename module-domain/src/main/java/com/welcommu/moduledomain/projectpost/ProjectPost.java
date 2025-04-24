faaac57e8698283d49e7b90c84babf9063245f2c
moosoobi <tmxkcksgh@gmail.com>
2025-04-24 17:59:41 +0900

    [feature] ProjectPost에서 Project Fetchtype Eager로 변경

--- module-domain/src/main/java/com/welcommu/moduledomain/projectpost/ProjectPost.java
+++ module-domain/src/main/java/com/welcommu/moduledomain/projectpost/ProjectPost.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -60,7 +59,7 @@ public class ProjectPost {
     @JoinColumn(name = "creator_id", referencedColumnName = "id", nullable = false)
     private User creator;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "project_id", referencedColumnName = "id", nullable = false)
     private Project project;
 
