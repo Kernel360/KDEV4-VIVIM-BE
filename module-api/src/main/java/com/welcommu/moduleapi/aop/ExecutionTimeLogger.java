8bc7c7d8c68af3d07f6ce94208c3802eb63a288a
HoyiTT <pjs9177@naver.com>
2025-04-25 15:03:02 +0900

    [fix] ExecutionTimeLogger를 활성화 비활성화가 가능하게 수정

--- module-api/src/main/java/com/welcommu/moduleapi/aop/ExecutionTimeLogger.java
+++ module-api/src/main/java/com/welcommu/moduleapi/aop/ExecutionTimeLogger.java
@@ -1,25 +1,32 @@
 package com.welcommu.moduleapi.aop;
 
-
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class ExecutionTimeLogger {
+
+    @Value("${logging.execution-time.enabled:false}")
+    private boolean enabled;
+
     @Around("execution(* com.welcommu.moduleapi..*Controller.*(..)) || execution(* com.welcommu.moduleservice..*Service.*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (!enabled) {
+            return joinPoint.proceed();
+        }
+
         long start = System.currentTimeMillis();
-
         Object result = joinPoint.proceed();
-
         long duration = System.currentTimeMillis() - start;
-        log.info("⏱️ {} executed in {} ms", joinPoint.getSignature(), duration);
 
+        log.info("⏱️ {} executed in {} ms", joinPoint.getSignature(), duration);
         return result;
     }
 }
