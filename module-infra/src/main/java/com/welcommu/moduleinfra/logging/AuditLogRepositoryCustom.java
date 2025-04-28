fd9a4a22a83ae8b1e1c94d29f77b0ce57f2fe6ca
JUNSEO PARK <pjs9177@naver.com>
2025-04-28 14:08:08 +0900

    [refactor] #126 AuditLogSearch시 과도하게 시간이 소모되는 현상(N+1) 트러블 슈팅 + ExecutionTimeLogger 적용 + auditLog 페이징 적용 (#127)
    * [refactor] AuditLogSearch시 과도하게 시간이 소모되는 현상 트러블 슈팅
    
    * [fix] ExecutionTimeLogger를 활성화 비활성화가 가능하게 수정
    
    * [feature] auditLog Paging 적용

--- module-infra/src/main/java/com/welcommu/moduleinfra/logging/AuditLogRepositoryCustom.java
+++ module-infra/src/main/java/com/welcommu/moduleinfra/logging/AuditLogRepositoryCustom.java
@@ -3,15 +3,19 @@ package com.welcommu.moduleinfra.logging;
 import com.welcommu.moduledomain.logging.AuditLog;
 import com.welcommu.moduledomain.logging.enums.ActionType;
 import com.welcommu.moduledomain.logging.enums.TargetType;
+
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AuditLogRepositoryCustom {
-    List<AuditLog> findByConditions(
+    Page<AuditLog> findByConditions(
         ActionType actionType,
         TargetType targetType,
         LocalDateTime startDate,
         LocalDateTime endDate,
-        Long userId
+        Long userId,
+        Pageable pageable
     );
 }
