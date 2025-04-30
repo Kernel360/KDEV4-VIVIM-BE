9a57126f01595fe5222a27240b572dd6b92fae59
HoyiTT <pjs9177@naver.com>
2025-04-30 14:53:18 +0900

    [refactor] ProjectAuditService를 인터페이스로 분리

---
+++ module-service/src/main/java/com/welcommu/moduleservice/project/audit/ProjectAuditServiceImpl.java
@@ -0,0 +1,43 @@
+package com.welcommu.moduleservice.project.audit;
+
+
+import com.welcommu.moduledomain.logging.AuditLog;
+import com.welcommu.moduledomain.logging.enums.ActionType;
+import com.welcommu.moduledomain.logging.enums.TargetType;
+import com.welcommu.moduleinfra.logging.AuditLogRepository;
+import com.welcommu.moduleservice.logging.AuditLogFactory;
+import com.welcommu.moduleservice.logging.AuditLogFieldComparator;
+import com.welcommu.moduleservice.logging.AuditableService;
+import com.welcommu.moduleservice.project.dto.ProjectSnapshot;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@RequiredArgsConstructor
+@Service
+public class ProjectAuditServiceImpl implements AuditableService<ProjectSnapshot>, ProjectAuditService {
+
+    private final AuditLogRepository auditLogRepository;
+    private final AuditLogFieldComparator auditLogFieldComparator;
+    private final AuditLogFactory auditLogFactory;
+
+    @Override
+    public void createAuditLog(ProjectSnapshot project, Long userId) {
+        AuditLog log = auditLogFactory.create(TargetType.PROJECT, project.getId(), ActionType.CREATE, userId);
+        auditLogRepository.save(log);
+    }
+
+    @Override
+    public void modifyAuditLog(ProjectSnapshot before, ProjectSnapshot after, Long userId) {
+        Map<String, String[]> changedFields = auditLogFieldComparator.compare(before, after);
+        AuditLog log = auditLogFactory.createWithFieldChanges(TargetType.PROJECT, after.getId(), ActionType.MODIFY, userId,changedFields);
+        auditLogRepository.save(log);
+    }
+
+    @Override
+    public void deleteAuditLog(ProjectSnapshot project, Long userId) {
+        AuditLog log = auditLogFactory.create(TargetType.PROJECT, project.getId(), ActionType.DELETE, userId);
+        auditLogRepository.save(log);
+    }
+}
