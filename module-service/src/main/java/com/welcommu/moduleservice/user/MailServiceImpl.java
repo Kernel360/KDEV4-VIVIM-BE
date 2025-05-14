341a6e1ad473be73c27a03800bcea4285b2bae20
HoyiTT <pjs9177@naver.com>
2025-05-14 12:19:00 +0900

    [fix] 재처리 로직 추가

--- module-service/src/main/java/com/welcommu/moduleservice/user/MailServiceImpl.java
+++ module-service/src/main/java/com/welcommu/moduleservice/user/MailServiceImpl.java
@@ -4,8 +4,12 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class MailServiceImpl implements MailService{
     private final JavaMailSender mailSender;
+
+    @Retryable(
+        value = { MailSendException.class },   // 재시도 대상 예외
+        maxAttempts = 3,                       // 총 3번 시도
+        backoff = @Backoff(delay = 5000, multiplier = 2)
+    )
 
     @Async
     public void sendEmail(String to, String subject, String htmlBody) {
@@ -27,5 +37,11 @@ public class MailServiceImpl implements MailService{
         } catch (MessagingException e) {
             log.error("메일 전송 실패(to: {})", to, e);
         }
+    }
+
+    @Recover
+    public void recover(MailSendException e, String to, String subject, String htmlBody) {
+        log.error("메일 전송 재시도 모두 실패: to={}, subject={}", to, subject, e);
+        // TODO: DB에 실패 로그 남기기, 관리자 알림
     }
 }
