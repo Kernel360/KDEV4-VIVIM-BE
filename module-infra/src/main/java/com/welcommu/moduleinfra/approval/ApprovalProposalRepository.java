869033b15a80c6bd51d74005f373756e87f5a10e
EOJIN <122079064+song-eojin@users.noreply.github.com>
2025-05-15 12:35:32 +0900

    [refactor] ApprovalProposal 최근 승인요청 프로젝트 참여자만 조회되도록 개선 (#194)
    * [feature] 프로젝트 진행단계별 승인요청 승인률 조회 기능 구현
    
    * [feature] 프로젝트 진행단계별 승인요청 승인률 조회 기능 구현
    
    * [refactor] 최근 승인요청 프로젝트 참여자만 조회되도록 개선
    
    * [refactor] 최근 승인요청 프로젝트 참여자만 조회 여부 확인

--- module-infra/src/main/java/com/welcommu/moduleinfra/approval/ApprovalProposalRepository.java
+++ module-infra/src/main/java/com/welcommu/moduleinfra/approval/ApprovalProposalRepository.java
@@ -3,9 +3,13 @@ package com.welcommu.moduleinfra.approval;
 import com.welcommu.moduledomain.approval.ApprovalProposal;
 import com.welcommu.moduledomain.approval.ApprovalProposalStatus;
 import com.welcommu.moduledomain.projectprogress.ProjectProgress;
+import com.welcommu.moduledomain.user.User;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,9 +17,16 @@ public interface ApprovalProposalRepository extends JpaRepository<ApprovalPropos
 
     List<ApprovalProposal> findByProjectProgress(ProjectProgress progress);
 
-    Long countByProjectProgressIdAndProposalStatus(Long id, ApprovalProposalStatus approvalProposalStatus);
+    Long countByProjectProgressIdAndProposalStatus(Long id,
+        ApprovalProposalStatus approvalProposalStatus);
 
     Long countByProjectProgressId(Long id);
 
-    List<ApprovalProposal> findAllByOrderByCreatedAtDesc(PageRequest createdAt);
+    @Query("""
+    SELECT ap
+    FROM ApprovalProposal ap
+    WHERE ap.projectProgress.project.id IN :projectIds
+    ORDER BY ap.createdAt DESC
+""")
+    List<ApprovalProposal> findRecentProposalsByProjectIds(@Param("projectIds") List<Long> projectIds, Pageable pageable);
 }
